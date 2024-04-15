96d1db5f3f62bbf916851c37a64bfea0053029ee
Javier Ballón <javi4195@gmail.com>
2024-04-15 12:17:46 -0300

    Script for manipulating fruits

---
+++ Fruit Ninja/Assets/Scripts/Fruits.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruits : MonoBehaviour
+{
+    public GameObject prefabCutFruit;
+
+    public void CreateCutFruit()
+    {
+        GameObject inst = (GameObject)Instantiate(prefabCutFruit, transform.position, transform.rotation);
+
+        Rigidbody[] rbsCuts = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody r in rbsCuts)
+        {
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+        }
+
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateCutFruit();
+        }
+    }
+}
