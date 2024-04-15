5ea57e11ba0e115fc8e77dd8ddc46a95e5b22773
Javier Ballón <javi4195@gmail.com>
2024-04-15 17:31:24 -0300

    Modified sword script prepared for Android touch

--- Fruit Ninja/Assets/Scripts/Sword.cs
+++ Fruit Ninja/Assets/Scripts/Sword.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    public float speedMin = 0.1f;
+
     private Rigidbody2D rb;
+    private Vector3 lastPositionMouse;
+    private Vector3 speedMouse;
+
+    private Collider2D col;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        col.enabled = MouseMovement();
         ConnectSowrdToMouse();
     }
 
@@ -23,5 +31,21 @@ public class Sword : MonoBehaviour
         mousePosition.z = 10;
 
         rb.position = Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private bool MouseMovement()
+    {
+        Vector3 positionMouse = transform.position;
+        float movement = (lastPositionMouse - positionMouse).magnitude;
+        lastPositionMouse = positionMouse;
+
+        if(movement > speedMin)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
