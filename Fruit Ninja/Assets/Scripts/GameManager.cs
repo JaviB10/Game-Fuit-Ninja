8fe2a9265773f2f8e0d7fe0ce61216b0da7e6e77
Javier Ballón <javi4195@gmail.com>
2024-04-15 16:36:51 -0300

    Script for best score configuration

--- Fruit Ninja/Assets/Scripts/GameManager.cs
+++ Fruit Ninja/Assets/Scripts/GameManager.cs
@@ -5,17 +5,59 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Score elements")]
     public int score;
+    public int bestScore;
     public Text textScore;
+    public Text textBestScore;
+
+    [Header("Game Over elements")]
+    public GameObject gameOverPanel;
+    public Text textFinalScore;
+    public Text textFinalBestScore;
+
+    private void Awake()
+    {
+        gameOverPanel.SetActive(false);
+        SetBestScore();
+    }
+
+    private void SetBestScore()
+    {
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        textBestScore.text = "BEST: " + bestScore.ToString();
+    }
 
     public void IncreaseScore()
     {
         score += 2;
         textScore.text = score.ToString();
+
+        if(score > bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            textBestScore.text = "BEST: " + score.ToString();
+        }
     }
 
     public void ToTouchBomb()
     {
         Time.timeScale = 0;
+        textFinalScore.text = "Score: " + score.ToString();
+        gameOverPanel.SetActive(true);
+        textFinalBestScore.text = "BEST SCORE: " + bestScore.ToString();
+    }
+
+    public void Reset()
+    {
+        score = 0;
+        textScore.text = score.ToString();
+        Time.timeScale = 1;
+        gameOverPanel.SetActive(false);
+
+        foreach (GameObject g in GameObject.FindGameObjectsWithTag("Interactive"))
+        {
+            Destroy(g);
+        }
     }
 }
